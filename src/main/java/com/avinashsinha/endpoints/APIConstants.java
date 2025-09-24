29cddb4033b5f85d8f14bfcd2992dd7c2c395d76
HP <sinha.avinash906@gmail.com>
2025-09-24 15:08:32 +0530

    API Automation Framework Restful Booker

---
+++ src/main/java/com/avinashsinha/endpoints/APIConstants.java
@@ -0,0 +1,12 @@
+package com.avinashsinha.endpoints;
+
+// Here, include all the URLs that are common across all
+
+public class APIConstants {
+
+    public static String BASE_URL = "https://restful-booker.herokuapp.com";
+    public static String BASE_PATH = "/booking";
+    public static String AUTH_URL = "/auth";
+    public static String PING_URL = "/ping";
+
+}

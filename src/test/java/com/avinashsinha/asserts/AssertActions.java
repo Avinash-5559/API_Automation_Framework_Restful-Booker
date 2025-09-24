4210f29fe70d65471378a00a4d856d42e0fee4a0
HP <sinha.avinash906@gmail.com>
2025-09-24 15:09:28 +0530

    API Automation Framework Restful Booker

---
+++ src/test/java/com/avinashsinha/asserts/AssertActions.java
@@ -0,0 +1,35 @@
+package com.avinashsinha.asserts;
+
+import io.restassured.response.Response;
+
+import static org.testng.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.*;
+
+public class AssertActions {
+
+    public void verifyResponseBody(String actual, String expected, String description) {
+        assertEquals(actual, expected, description);
+    }
+
+    public void verifyResponseBody(int actual, int expected, String description) {
+        assertEquals(actual, expected, description);
+    }
+
+    public void verifyStatusCode(Response response, Integer expected){
+        assertEquals(response.getStatusCode(),expected);
+    }
+
+    public void verifyStringKey(String keyExpect, String keyActual){
+        assertThat(keyExpect).isNotNull().isNotBlank().isNotEmpty();
+        assertThat(keyExpect).isEqualTo(keyActual);
+    }
+
+    public void verifyIntegerKeyNotNull(Integer keyExpect){
+        assertThat(keyExpect).isNotNull();
+    }
+
+    public void verifyStringKeyNotNull(String keyExpect){
+        assertThat(keyExpect).isNotNull();
+    }
+
+}
